a33b3c96d4c52bfe30ffca0c4eedf2bc29a365af
入木田　萌 <moe_irikida@dwango.co.jp>
2018-06-21 17:44:46 +0900

    lambda プログラム完了

--- lambda-jikken/lambda-jikken.md
+++ lambda-jikken/lambda-jikken.md
@@ -22,7 +22,27 @@
   "message": "Internal server error"
 }
 ```
+> Amazon API Gateway を使用して HTTP リクエストに応答してコードを実行します。
+- lambda を HTTP から呼び出すのが、API gateway か。そんで、json を返すのか。
 
-- lambda に param を渡すのが、API gateway か。そんで、json を返すのか。で、合ってるかな？
+### API に json を食わせる @ GUI
+
+- テストとして、json を食わせている
+- 本当はテストではなく、ただ呼び出すと API が返るようにしたい
+- 静的 API となるが、これ意味あるの？
+- パラメタによってレスポンスを変えるようにした(なんとなくしか作れないなー、業務で使う勘所がつかめない)
 
 ### cui(cli) で動かしたい
+
+```
+$ aws lambda create-function \
+...
+```
+
+- role などの設定が変わってしまったので、来週続きをする
+
+### サーバレスアプリで db を使いたい
+
+- dynamoDB を調べる
+- 何が違う？DynamoDBとRDS > http://blog.serverworks.co.jp/tech/2017/04/12/what_is_different_dynamodb_and_rds/
+- いまいち -> amazon RDS 使ってみる
--- lambda-jikken/lambda_function.py
+++ lambda-jikken/lambda_function.py
@@ -1,9 +1,15 @@
 def lambda_handler(event, context):
-    for i in event["houshin"]:
-        print (i["character"])
-        print("<a href='https://ja.wikipedia.org/wiki/" + i["voice"] + "'>")
-        print(i["voice"])
-        print("</a>¥n")
-    return 0
-
-def get_seiyu_json():
+    import json
+    f = open('seiyu.json')
+    data = json.load(f)
+    f.close()
+    if event["title"] == "houshin":
+        ret = "hakyu houshin-engi"
+        for i in data["houshin"]:
+            print (i["character"])
+            print("<a href='https://ja.wikipedia.org/wiki/" + i["voice"] + "'>")
+            print(i["voice"])
+            print("</a>¥n")
+    else:
+        ret = "other title¥n"
+    return ret
